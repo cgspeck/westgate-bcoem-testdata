2ab2872f4d52da78f5a9e69646ffd84b6cab0c4f
Chris Speck <cgspeck@gmail.com>
2025-04-20 20:11:12 +1000

    Remove unused code

--- src/config.py
+++ src/config.py
@@ -5,12 +5,6 @@ min_judges=40
 max_entries_per_participant=8
 max_entries_per_participant_per_group=2
 # Probabilities
-## Entries
-# TODO: confirm unused and delete below
-p_entry_porter=0.5
-p_entry_stout=0.7
-p_entry_strong_stout=0.5
-p_entry_specialty_stout=0.7
 ## Participants
 p_participant_has_entry=0.6
 p_participant_is_judge=0.6
