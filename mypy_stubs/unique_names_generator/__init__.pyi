fdef0885f78d1aa3ac73879280c1ebce410280dd
Chris Speck <cgspeck@gmail.com>
2025-04-20 19:58:16 +1000

    Add stubs and mypy config

---
+++ mypy_stubs/unique_names_generator/__init__.pyi
@@ -0,0 +1,8 @@
+from _typeshed import Incomplete
+
+full_path: Incomplete
+FILES: Incomplete
+
+def get_random_name(
+    combo=[list[str], list[str]], separator: str = " ", style: str = "capital"
+): ...
